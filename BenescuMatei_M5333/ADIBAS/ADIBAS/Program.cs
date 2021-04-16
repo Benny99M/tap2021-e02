27ae995bc3d4103155d4a3aeec89eb5ccaea7f06
Benny99M <benescumatei@yahoo.com>
2021-04-16 09:02:58 +0300

    Added DnaTest and Solid.txt

---
+++ BenescuMatei_M5333/ADIBAS/ADIBAS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADIBAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample random= new Sample()
+        }
+    }
+}
---
+++ BenescuMatei_M5333/ADIBAS/ADIBAS/Sample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADIBAS
+{
+    class Sample
+    {
+        Guid Id;
+        string DnaSequence;
+        DateTime AnalysisTime;
+        protected Sample(Guid Id, string DnaSequence, DateTime AnalysisTime) : this(Id, DnaSequence, AnalysisTime)
+        {
+
+        }
+        public Guid Id { get; private set; }
+        public string DnaSequence { get; private set; }
+        public DateTime AnalysisTime { get; private set; }
+
+    }
+}
