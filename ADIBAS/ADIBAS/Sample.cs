a7b291907ab04ff24e4dcaa256044f48a8a19d7a
Benny99M <benescumatei@yahoo.com>
2021-04-16 08:12:27 +0300

    Added Sample.cs and Repository pattern

---
+++ ADIBAS/ADIBAS/Sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADIBAS
+{
+    class Sample
+    {
+        Guid Id;
+        String DnaSequence;
+        DateTime AnalysisTime;
+
+    }
+}
